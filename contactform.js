3816783d39595e3ed3e368b442ef4dd08004dc76
Elizabeth Renderos <elirenderos@gmail.com>
2021-09-23 06:59:00 -0500

    created new contact form design page for website

---
+++ contactform.js
@@ -0,0 +1,13 @@
+document.body.style.backgroundImage = "url('https://app.funnelmaker.com/users/myteam33104/Media2320.png')";
+document.body.style.backgroundRepeat = "no-repeat";
+
+
+const submitform = document.getElementById('#submitform')
+
+
+submitform.addEventListener('click', () => {
+    
+})
+
+
+
