df24b26e3663dbb73f2dc0da4fcf23db10720556
Elizabeth Renderos <elirenderos@gmail.com>
2021-10-30 09:08:40 -0500

    created the contact form-first draft

--- contactform.js
+++ contactform.js
@@ -1,13 +1,3 @@
-document.body.style.backgroundImage = "url('https://app.funnelmaker.com/users/myteam33104/Media2320.png')";
-document.body.style.backgroundRepeat = "no-repeat";
-
-
-const submitform = document.getElementById('#submitform')
-
-
-submitform.addEventListener('click', () => {
-    
-})
 
 
 
